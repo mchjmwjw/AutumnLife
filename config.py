10e9a60becd1c135f2e81f3ad8c8a729a1760c45
mchjmwjw <mchjmwjw@163.com>
2017-03-07 17:47:41 +0800

    项目部署完成测试

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+#coding=utf8
 import os
 import platform
 
@@ -6,12 +7,12 @@ basedir = path.abspath(path.dirname(__file__))
 cursystem = platform.system()
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or 'whats your name'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or 'whats your name' # 秘钥
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    TUNTUNPY_MAIL_SUBJECT_PREFIX = '[AutumnLife]'
-    TUNTUNPY_MAIL_SENDER = 'Hoster Wang <' + str(os.environ.get('MAIL_USERNAME')) + '>'
-    TUNTUNPY_ADMIN = os.environ.get('AUTUMNLIFE_ADMIN')
+    AUTUMNLIFE_MAIL_SUBJECT_PREFIX = '[AutumnLife]' # 邮件名称抬头
+    AUTUMNLIFE_MAIL_SENDER = 'Hoster Wang <' + str(os.environ.get('MAIL_USERNAME')) + '>' # 邮件发送人
+    AUTUMNLIFE_ADMIN = os.environ.get('AUTUMNLIFE_ADMIN')
 
     @staticmethod
     def init_app(app):
@@ -26,20 +27,19 @@ class DevelopmentConfig(Config):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-                              cursystem is 'Windows' and 'sqlite:///' + os.path.join(basedir, \
-                                                                                     'dbs/tuntunpy.db') or 'sqlite:////' + os.path.join(
-        basedir, 'dbs/tuntunpy.db')
+                cursystem is 'Windows' and 'sqlite:///' + os.path.join(basedir, \
+                'dbs/tuntunpy.db') or 'sqlite:////' + os.path.join(basedir, 'dbs/tuntunpy.db')
 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+                              'sqlite:///' + os.path.join(basedir, 'dbs/data-test.sqlite')
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+                              'sqlite:///' + os.path.join(basedir, 'dbs/data.sqlite')
 
 
 config = {
