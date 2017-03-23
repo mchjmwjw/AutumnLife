b872b804e633f8d34b830f80fb8deb99b39282e8
mchjmwjw <mchjmwjw@163.com>
2017-03-23 19:54:30 +0800

    添加用户资料，界面不可用

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+#coding=utf8
 from flask import render_template
 from . import main
 import os
@@ -12,3 +13,11 @@ def index():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/icon')
+
+# 用户资料
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
--- app/models.py
+++ app/models.py
@@ -1,29 +1,58 @@
 #coding=utf8
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin   # 管理用户认证系统，认证状态
+from flask_login import UserMixin, AnonymousUserMixin   # 管理用户认证系统，认证状态
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+from datetime import *
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer) # 位标志，每一位都表示一种权限
 
     def __repr__(self):
         return '<Role %r>' % self.name
+    
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permission.FOLLOW |
+                     Permission.COMMENT |
+                     Permission.WRITE_ARTICLE, True),   # (权限, 是否默认)
+            'Moderator': (Permission.FOLLOW |
+                          Permission.COMMENT |
+                          Permission.WRITE_ARTICLE |
+                          Permission.MODERATE_COMMENTS, True),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(64), unique = True, index = True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     sex = db.Column(db.Integer, default=1)
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(),  default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     
     @property
     def password(self):     # 试图读取password的值将会报错
@@ -54,7 +83,41 @@ class User(UserMixin, db.Model):
         self.confirmed = True   # 验证成功
         db.session.add(self)    # 添加用户
         return True
+    
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)    # 调用父类的构造方法
+        if self.role is None:
+            if self.email == current_app.comfig['AUTNMULIFE_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()  # 管理员角色
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()  # 非管理员角色
+    
+    def can(self, permissions):
+        return self.role is not None and \
+               (self.role.permissions & permissions) == permissions
+    def is_administrator(self):
+        return self.can(Permission.ADMINISTER)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
 
 @login_manager.user_loader
 def load_user(user_id):         # 加载用户的回调函数
     return User.query.get(int(user_id))
+
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLE = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
+    
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+    
+    def is_administrator(self):
+        return False
+    
+login_manager.anonymous_user = AnonymousUser
